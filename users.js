2bfdd6c6bcb2aca65b1e9822981638e53f5f6151
Ryan Burnette <ryan.burnette@gmail.com>
2020-05-18 15:54:45 -0400

    refactor users

--- users.js
+++ users.js
@@ -3,23 +3,17 @@
 var clone = require('lodash.clonedeep');
 
 module.exports = function (users) {
-  var obj = {};
-
   var _users = clone(users);
 
-  obj.all = async function () {
-    return _users.map(function (user) {
-      return clone(user);
-    });
-  };
+  async function all() {
+    return clone(_users);
+  }
 
-  obj.find = async function (email) {
-    return obj.all().then(function (_users) {
-      return _users.find(function (user) {
-        return user.email === email;
-      });
+  async function find(email) {
+    return all().then(function (users) {
+      return users.find((u) => email === u.email);
     });
-  };
+  }
 
-  return obj;
+  return { all, find };
 };
